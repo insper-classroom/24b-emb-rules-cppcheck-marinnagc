bb5beea365e41e452a1ae6177163645f2fded714
Marinna <marinnagc@al.insper.edu.br>
2024-08-19 11:02:10 -0300

    arrumando

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 5; 
+    int b = 5; //uninitvar, legacyUninitvar
+    int result = 0;
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
     }
 
